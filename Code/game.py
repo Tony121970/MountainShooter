97548125624a6bdf766b34573f9797147ca38573
TonyMarino <tonymarino00@hotmail.com>
2025-08-18 23:36:01 -0300

    Adding classes generated from StarUML

--- Code/game.py
+++ Code/game.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-class Menu:
-    def __init__(self, window):
-        self.window = window
+class Game:
+    def __init__(self):
+        self.window = None
 
     def run(self, ):
         pass
